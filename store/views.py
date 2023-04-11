d05e6ccad9ae0760bb936259ad8c8b243968a6c3
muhameddadel <muhameddadel2000@gmail.com>
2023-04-12 00:55:23 +0200

    categories view

--- store/views.py
+++ store/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import *
 
 
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
 def all_products(request):
     products = Product.objects.all()
 
