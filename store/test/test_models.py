6034459926ca992b62fd97790ecc56603ee1c1b0
muhameddadel <muhameddadel2000@gmail.com>
2023-04-11 19:22:52 +0200

    models test

--- store/test/test_models.py
+++ store/test/test_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from store.models import *
 
 
@@ -12,3 +13,26 @@ class TestCategoriesModel(TestCase):
         """
         data = self.detal
         self.assertTrue(isinstance(data, Category))
+
+    def test_category_model_entry(self):
+        """
+        Test Category model return name
+        """
+        data = self.detal
+        self.assertEqual(str(data), 'django')
+
+
+class TestProductsModel(TestCase):
+    def setUp(self):
+        Category.objects.create(name = 'django', slug = 'django')
+        User.objects.create(username = 'admin')
+        self.data1 = Product.objects.create(category_id = 1, title = 'django beginners', created_by_id = 1,
+                                            slug = 'django-beginners', price = '20.00', image = 'django' )
+        
+    def test_products_model_entry(self):
+        """
+        Test Product model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), 'django beginners')
