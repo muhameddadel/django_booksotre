00306de2441ab3646c0cf11994a73f4c2f4ca912
muhameddadel <muhameddadel2000@gmail.com>
2023-04-21 04:47:11 +0200

    basket_add view

--- basket/views.py
+++ basket/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
+from store.models import *
+from .basket import Basket
 
 def basket_summary(request):
     return render(request, 'store/basket/summary.html')
 
 
 def basket_add(request):
-    pass
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product)
