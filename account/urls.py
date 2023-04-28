35c8af81b12c9991634221af7678b12bfd625448
muhameddadel <muhameddadel2000@gmail.com>
2023-04-28 22:16:13 +0300

    login template

--- account/urls.py
+++ account/urls.py
@@ -7,7 +7,7 @@ from .views import *
 app_name = 'account'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='account/regsitartion/login.html', form_class=UserLoginForm), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html', form_class=UserLoginForm), name='login'),
     path('register/', account_register, name='register'),
     path('activate/<slug:uidb64>/<slug:token>', account_activate, name='activate'),
     path('dashboard/', dashboard, name='dashboard'),
