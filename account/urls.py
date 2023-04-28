cdd03627a4e50db0986f3c23c91a8c342ac9c9fe
muhameddadel <muhameddadel2000@gmail.com>
2023-04-28 21:57:23 +0300

    login form

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,13 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
+from .forms import *
 from .views import *
 
 app_name = 'account'
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='account/regsitartion/login.html', form_class=UserLoginForm), name='login'),
     path('register/', account_register, name='register'),
     path('activate/<slug:uidb64>/<slug:token>', account_activate, name='activate'),
     path('dashboard/', dashboard, name='dashboard'),
