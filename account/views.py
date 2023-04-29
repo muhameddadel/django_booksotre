aee99a623e98f7fae19b2b5877b48c9ea6696b81
muhameddadel <muhameddadel2000@gmail.com>
2023-04-29 03:06:39 +0300

    user edit form

--- account/views.py
+++ account/views.py
@@ -7,7 +7,7 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
-from .forms import RegistrationFrom
+from .forms import *
 from .models import UserBase
 from .token import account_activation_token
 
