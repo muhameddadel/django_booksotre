d10799246ff24a200a7d523a2d3fe1d8e47c853a
muhameddadel <muhameddadel2000@gmail.com>
2023-05-02 13:54:05 +0300

    stripe

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,23 @@
+import stripe
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
+from basket.basket import Basket
 
 
 @login_required
 def BasketView(request):
-    return render(request, 'payment/home.html')
+    basket = Basket(request)
+    total = str(basket.get_total_price())
+    total = total.replace('.', '')
+    
+    stripe.api_key = 'sk_test_51N3ElZHCEFgrfiGmEfrNWjSLGSnLzUKzuZW24hAFswSlAfuIW0DFnKT8i6zbHObHZALyyPGddwc8K8MQyWTwF3jk00JUBvFC1C'
+    intent = stripe.PaymentIntent.create(
+        amount=total,
+        currency='gbp',
+        metadata={'userid': request.user.id}
+    )
+
+    context = {'client_secret': intent.client_secret}
+    return render(request, 'payment/home.html', context)
 
