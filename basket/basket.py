798e805cc9ba5fc31b30b54ed9eb973a9b05476b
muhameddadel <muhameddadel2000@gmail.com>
2023-05-07 18:08:09 +0300

    edit template

--- basket/basket.py
+++ basket/basket.py
@@ -26,7 +26,7 @@ class Basket():
         if product_id in self.basket:
             self.basket[product_id]['qty'] = qty
         else:
-            self.basket[product_id] = {'price': str(product.price), 'qty': qty}
+            self.basket[product_id] = {'price': str(product.regular_price), 'qty': qty}
 
         self.save()
 
@@ -36,7 +36,7 @@ class Basket():
         and return products
         """
         product_ids = self.basket.keys()
-        prodcuts = Product.products.filter(id__in = product_ids)
+        prodcuts = Product.objects.filter(id__in = product_ids)
         basket = self.basket.copy()
 
         for product in prodcuts:
--- store/views.py
+++ store/views.py
@@ -4,20 +4,20 @@ from .models import *
 
 
 def all_products(request):
-    products = Product.objects.all()
+    products = Product.objects.prefetch_related("product_image").filter(is_active=True)
     context = {'products': products}
     return render(request, 'store/index.html',context )
 
 
 def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    product = get_object_or_404(Product, slug=slug, is_active=True)
     context = {'product': product}
     return render(request, 'store/detail.html', context)
 
 
 def category_list(request, category_slug):
     category = get_object_or_404(Category, slug = category_slug)
-    products = Product.products.filter(category= category)
+    products = Product.objects.filter(category__in=Category.objects.get(name=category_slug).get_descendants(include_self=True))
 
     context = {'category': category, 'products': products}
 
