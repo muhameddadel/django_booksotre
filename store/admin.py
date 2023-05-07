b6c84aab600f198bc01c1f969ebfe8ff1340076d
muhameddadel <muhameddadel2000@gmail.com>
2023-05-07 17:14:37 +0300

    register models

--- store/admin.py
+++ store/admin.py
@@ -1,18 +1,35 @@
+from django import forms
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
 from .models import *
 
 
-# @admin.register(Category)
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ['name', 'slug']
-#     prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Category, MPTTModelAdmin)
 
 
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = ['title', 'author', 'slug', 'price', 'in_stock', 'created', 'updated']
-#     list_filter = ['in_stock'] 
-#     list_editable = ['price', 'in_stock']
-#     prepopulated_fields = {'slug': ('title',)}
-    
+class ProductSpecificationInline(admin.TabularInline):
+    model = ProductSpecification
+
+
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductSpecificationInline,
+    ]
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+
+class ProductSpecificationValueInline(admin.TabularInline):
+    model = ProductSpecificationValue
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductSpecificationValueInline,
+        ProductImageInline,
+    ]
